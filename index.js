e5259e69f6774daa1bf6103f424f986691c3f7ab
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2021-06-21 10:02:07 +0300

    Add app.listen(process.env.PORT || 3000);

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const URL = "http://rsp.iseu.by/Raspisanie/TimeTable/umu.aspx";
 
+app.listen(process.env.PORT || 3000);
+
 app.listen(port, function () {
   console.log("Server start successfully");
 });
