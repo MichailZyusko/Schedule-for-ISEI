9bbe3e393d38ca7347c4d3c812a9cf716d1c3fa4
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2021-10-03 21:17:47 +0300

    Create prjoect

--- src/public/components/index.js
+++
@@ -1,4 +0,0 @@
-import pageLoading from './pageLoading/controller.js';
-import showButtons from './buttons/show/controller.js'
-
-export { pageLoading, showButtons };
--- src/public/index.js
+++
@@ -1,6 +0,0 @@
-import { pageLoading, showButtons } from './components/index.js';
-
-const showButton = document.getElementById('showButton');
-
-document.addEventListener('DOMContentLoaded', pageLoading);
-showButton.addEventListener('click', showButtons);
