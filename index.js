1bdc05e0b5f78b9f2fea9c494f75ab8e929bacb9
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2021-10-04 09:25:48 +0300

    'v1.0.7'

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const PORT = process.env.PORT || 3000;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  console.log(`v1.0.6 Example app listening at http://${host}:${PORT}`);
-  res.send({ message: `v1.0.6 Example app listening at http://${host}:${PORT}` });
+  console.log(`v1.0.7 Example app listening at http://${host}:${PORT}`);
+  res.send({ message: `v1.0.7 Example app listening at http://${host}:${PORT}` });
 });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/app', express.static('src/public'));
-app.use('/', router);
+app.use('/api/', router);
 
 app.listen(PORT, host, () => {
   console.log(`Example app listening at http://${host}:${PORT}`);
