2bd549291066ac0fa3b45d58e3b19adb86c80a28
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2021-10-04 09:37:45 +0300

    API v1.0.0

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ import router from './src/router/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const host = '0.0.0.0';
+const HOST = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  console.log(`v1.0.7 Example app listening at http://${host}:${PORT}`);
-  res.send({ message: `v1.0.7 Example app listening at http://${host}:${PORT}` });
+  console.log(`v1.0.7 Example app listening at http://${HOST}:${PORT}`);
+  res.send({ message: `v1.0.7 Example app listening at http://${HOST}:${PORT}` });
 });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/app', express.static('src/public'));
 app.use('/api/', router);
 
-app.listen(PORT, host, () => {
-  console.log(`Example app listening at http://${host}:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Example app listening at http://${HOST}:${PORT}`);
 });
