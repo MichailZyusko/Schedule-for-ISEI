089fe4a775cff6dd70524d16b473bd90419a12e9
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2021-10-04 21:10:59 +0300

    'v1.0.18'

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+};
+app.use(allowCrossDomain);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
