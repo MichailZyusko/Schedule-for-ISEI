f36740eebb360289df283f68c92524f556c361a7
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2021-10-04 20:44:21 +0300

    'v1.0.18'

--- src/middleware/schedule/controller.js
+++ src/middleware/schedule/controller.js
@@ -23,7 +23,11 @@ export default async (req, res) => {
 
     const browser = await puppeteer.launch({
       headless: true,
-      args: ['- no-sandbox', '–disable-setuid-sandbox'],
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+      ],
     });
     const page = await browser.newPage();
 
