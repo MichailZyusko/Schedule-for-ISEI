48baeed19c3e7c5cdab0f4faf0a9dcb5b9f813f2
Michail Zyusko <67417124+MichailZyusko@users.noreply.github.com>
2021-10-09 18:06:00 +0300

    v1.0.21

--- src/middleware/metainfo/helper/getOptionsFromSelect.js
+++ src/middleware/metainfo/helper/getOptionsFromSelect.js
@@ -2,5 +2,5 @@ export default ($, selector) => $(selector)[0]
   .children.filter((elem) => elem.name === 'option')
   .map((elem) => ({
     id: elem.attribs.value,
-    value: elem.children[0].data.trim(),
+    label: elem.children[0].data.trim(),
   }));
